b73c0baf008391dc999e54c6f17b1b5aef39b026
madeitaly <matteo@madmap.uk>
2025-12-14 17:38:22 +0100

    add routing for POST '/user', POST '/new', POST '/add'

--- server.js
+++ server.js
@@ -51,6 +51,55 @@ app.get('/', async (req, res) => {
   });
 });
 
+app.post("/add", async (req, res) => {
+    const input = req.body['country'];
+    const currentUser = await getCurrentUser();
+
+    try{
+        const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'",
+            [input.toLowerCase()]
+        );
+
+        const data = result.rows[0];
+        const countryCode = data.country_code;
+
+        try{
+            await db.query("INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+                [countryCode, currentUserId]
+            );
+            res.redirect('/');
+        } catch(err) {
+            console.log(err);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+app.post('/user', async (req, res) => {
+    if(req.body.add === "new") {
+        res.render("new.ejs");
+    } else {
+        currentUserId = req.body.user;
+        res.redirect('/');
+    }
+})
+
+app.post('/new', async (req, res) => {
+   const name = req.body.name;
+   const color = req.body.color;
+   
+   const result = await db.query("INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
+    [name, color]
+   );
+
+   const id = result.rows[0].id;
+   currentUserId = id;
+
+   res.redirect('/');
+});
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
